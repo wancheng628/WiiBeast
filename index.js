81ace7e46eaced4932893b78348aa01708a2b2a5
Wancheng Piao <pwc628@gmail.com>
2017-01-31 23:22:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -28,8 +28,6 @@ var api = new ParseServer({
         production: false
       }
   },
-  verifyUserEmails: false,
-  publicServerURL: 'https://secret-coast-59488.herokuapp.com/parse/',
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
